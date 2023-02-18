0de23ddb32803a6958206fe57cd99e8b8b9f7187
maxb998 <102738073+maxb998@users.noreply.github.com>
2023-02-18 18:11:30 +0100

    Update main.c

--- 3/main.c
+++ 3/main.c
@@ -11,6 +11,8 @@
 //#include <sys/ipc.h>
 //#include <sys/types.h>
 
+#define MAX_THREADS 256
+
 
 typedef struct
 {
@@ -21,106 +23,178 @@ typedef struct
     bool quit;
 } SharedObj;
 
+typedef struct
+{
+    pthread_t threads[MAX_THREADS];
+    pthread_cond_t mutex;
+    pthread_cond_t consumeCond;
+} ThreadManagementValues;
+
+
+
 
 int main(int argc, char * argv[]);
 
-void producer(SharedObj * s);
+
+// init memory sharing to communicate with producer process
+SharedObj startMemSharing();
+
+// start the producer as a process and returns its pid
+pid_t startProducer();
+
+// producer task
+void producer();
+
+// start client's threads and return a struct containing values to manage those threads
 void startClients(int n);
-void client();
-void actor(SharedObj * s);
+
+// clients task
+void * client();
+
+// actor monitoring task, as a separate thread
+void * actor();
+
+
+char * buffer;
+size_t buffId;
+SharedObj s;
+ThreadManagementValues t;
+bool clientsQuit;
 
 
 
 int main(int argc, char * argv[])
 {
-    //  setup shared memory
-    int shmid = shmget(IPC_PRIVATE, sizeof(SharedObj),  SHM_R | SHM_W);
-
-    // check for errors
-    if (shmid == -1)
+    // PARSE ARGUMENTS: TODO
+    if (argc != 3)
     {
-        printf("Error on shmget\n");
-        exit(EXIT_FAILURE);
+        printf("Usage: main <BUFFER_SIZE> <CLIENTS_COUNT(THREADS)>");
+        exit(EXIT_SUCCESS);
     }
+    size_t bufferSize = 0, clientCount = 0;
+    sscanf("%d", argv[1], &bufferSize);
+    sscanf("%d", argv[2], &clientCount);
 
-    // points to the shared memory starting address
-    SharedObj * producer_actor = (SharedObj*)shmat(shmid, NULL, 0666);
+    // setup buffer
+    buffer = (char*)malloc(bufferSize * sizeof(char));
 
-    // check for errors
-    if ((!producer_actor))
-    {
-        printf("Error on shmat\n");
-        exit(EXIT_FAILURE);
-    }
+    // setup mem sharing
+    //s = startMemSharing();
 
-    // init mutex semaphore to avoid race condition between actor and producer process
-    sem_init(&producer_actor->mutexSem, 1, 1);
+    // start producer process and get its pid
+    //pid_t producerPid = startProducer(&s);
 
-    // init semaphores that regulates production rate
-    sem_init(&producer_actor->produceSem, 1, 0);
-    sem_init(&producer_actor->producedSem, 1, 1);
-    
-    // init other shared values
-    producer_actor->value = 0;
-    producer_actor->quit = false;
+    // now with the clients
+    clientsQuit = false;
+    startClients(clientCount);
 
-    // fork producer process
-    pid_t producerPid = fork();
-    if (producerPid == 0)
-    {
-        producer(producer_actor);
-        exit(EXIT_FAILURE);    // shouldn't be necessary
-    }
-
-    actor(producer_actor);
+    // finally the actor monitoring task on a separate thread
+    pthread_t actorThread;
+    pthread_create(&actorThread, NULL, actor, NULL);
 
     // wait producer to finish
-    waitpid(producerPid, NULL, 0);
+    //waitpid(producerPid, NULL, 0);
 
-    // print result
-    printf("%d\n", producer_actor->value);
+    free(buffer);
 
     return EXIT_SUCCESS;
 }
 
 
 
-void producer(SharedObj * s)
+SharedObj startMemSharing()
+{
+    // setup shared memory
+    int shmid = shmget(IPC_PRIVATE, sizeof(SharedObj),  SHM_R | SHM_W);
+    if (shmid == -1) // check for errors
+    {
+        printf("Error on shmget\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // points to the shared memory starting address
+    SharedObj * sl = (SharedObj*)shmat(shmid, NULL, 0666);
+    if ((!sl)) // check for errors
+    {
+        printf("Error on shmat\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return *sl;
+}
+
+pid_t startProducer()
+{
+    // init mutex semaphore to avoid race condition between actor and producer process
+    sem_init(&s.mutexSem, 1, 1);
+
+    // init semaphores that regulates production rate
+    sem_init(&s.produceSem, 1, 0);
+    sem_init(&s.producedSem, 1, 1);
+    
+    // init other shared values
+    s.value = 0;
+    s.quit = false;
+
+    // fork producer process
+    pid_t producerPid = fork();
+    if (producerPid == 0)
+    {
+        producer();
+        exit(EXIT_FAILURE);    // shouldn't be necessary but if it gets here there is an error
+    }
+
+    return producerPid;
+}
+
+void producer()
 {
     while(true)
     {
-        sem_wait(&s->produceSem);
-        sem_wait(&s->mutexSem);
+        sem_wait(&s.produceSem);
+        sem_wait(&s.mutexSem);
 
-        if (s->quit)
+        if (s.quit)
         {
-            sem_post(&s->producedSem);
-            sem_post(&s->mutexSem);
+            sem_post(&s.producedSem);
+            sem_post(&s.mutexSem);
             break;
         }
         else
         {
-            s->value++;
-            sem_post(&s->producedSem);
-            sem_post(&s->mutexSem);
+            s.value++;
+            sem_post(&s.producedSem);
+            sem_post(&s.mutexSem);
         }
     }
     exit(0);
 }
 
 void startClients(int n)
-{
+{  
+    // init mutex & conditions
+    pthread_mutex_init(&t.mutex, NULL);
+    pthread_cond_init(&t.consumeCond, NULL);
 
+    // start clients
+    for (int i = 0; i < n; i++)
+        pthread_create(t.threads[i], NULL, client(), NULL);
 }
 
-void client()
+void * client()
 {
+    while (!clientsQuit)
+    {
+        pthread_mutex_lock(&t.mutex);
 
+        
+    }
+    
 }
 
-void actor(SharedObj * s)
+void * actor()
 {
-    while (true)
+    /*while (true)
     {
         sem_wait(&s->producedSem);
         sem_wait(&s->mutexSem);
@@ -135,5 +209,5 @@ void actor(SharedObj * s)
         
         sem_post(&s->produceSem);
         sem_post(&s->mutexSem);
-    }
+    }*/
 }
